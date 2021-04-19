c38c6c6efac4165cb27670350d5cb7aa2e7a385a
sadra <sadrataghavi1383@gmail.com>
2021-04-19 14:11:41 +0430

    these are just extra stuff

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("add-contact/",views.addContact,name="addContact"),
 ]
